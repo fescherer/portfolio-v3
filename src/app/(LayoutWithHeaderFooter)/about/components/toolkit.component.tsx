4051c6a95c71ff75590d12096a8f0565fd7a25a3
unknown <felipescherer25@gmail.com>
2024-10-07 15:50:31 -0300

    :sparkles: feat: toolkit

--- src/app/(LayoutWithHeaderFooter)/about/components/toolkit.component.tsx
+++ src/app/(LayoutWithHeaderFooter)/about/components/toolkit.component.tsx
@@ -1,45 +1,8 @@
 import Image from "next/image";
 import { CardWrapper } from "./card-wrapper.component";
 import Link from "next/link";
-
-const toolkit = [
-  {
-    name: 'react',
-    icon: '/react.svg'
-  },
-  {
-    name: 'html',
-    icon: '/react.svg'
-  },
-  {
-    name: 'htmal',
-    icon: '/react.svg'
-  },
-  {
-    name: 'htmdsl',
-    icon: '/react.svg'
-  },
-  {
-    name: 'htmdl',
-    icon: '/react.svg'
-  },
-  {
-    name: 'htfdml',
-    icon: '/react.svg'
-  },
-  {
-    name: 'htmfdl',
-    icon: '/react.svg'
-  },
-  {
-    name: 'htmld',
-    icon: '/react.svg'
-  },
-  {
-    name: 'hawtml',
-    icon: '/react.svg'
-  },
-]
+import { toolkitData } from "@/util/data/toolkit.data";
+import { cn } from "@/util/cn.function";
 
 export function ToolkitComponent() {
   return (
@@ -50,22 +13,25 @@ export function ToolkitComponent() {
 
       <div className="flex flex-wrap items-center justify-center gap-4">
         {
-          toolkit.map(tool => (
+          toolkitData.map(tool => (
             <label key={tool.name} className="swap swap-flip">
               <input type="checkbox" />
 
               <div className="swap-off relative flex items-center justify-center rounded bg-neutral p-2 text-neutral-content">
-                <div className="flex flex-col items-center ">
-                  <Image width={40} height={40} alt={tool.name} src={tool.icon} />
+                <div className="flex flex-col items-center gap-2 ">
+
+                  <div className="rounded-md bg-white p-2">
+                    {tool.icon}
+                  </div>
                   {tool.name}
 
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="absolute -right-1 -top-1 size-4 rounded-full border border-neutral">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="absolute -right-1 -top-1 size-4 rounded-full  bg-base-content text-base-200">
                     <path fillRule="evenodd" d="M15 8A7 7 0 1 1 1 8a7 7 0 0 1 14 0ZM9 5a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM6.75 8a.75.75 0 0 0 0 1.5h.75v1.75a.75.75 0 0 0 1.5 0v-2.5A.75.75 0 0 0 8.25 8h-1.5Z" clipRule="evenodd" />
                   </svg>
                 </div>
               </div>
 
-              <div className="swap-on  relative flex max-w-32 flex-col items-center rounded bg-neutral p-2 text-neutral-content">
+              <div className="swap-on relative flex max-w-32 flex-col items-center rounded bg-neutral p-2 text-neutral-content">
                 <span className="text-xs">s sit diam amet magna. Proin gravida ullamcorper et dolor morbi proin euismod.</span>
 
                 <Link href="/" className="btn btn-circle btn-xs mt-4">
@@ -74,10 +40,10 @@ export function ToolkitComponent() {
                   </svg>
                 </Link>
 
-
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="absolute -right-1 -top-1 size-4 rounded-full border border-neutral">
-                  <path fillRule="evenodd" d="M14 8a.75.75 0 0 1-.75.75H4.56l3.22 3.22a.75.75 0 1 1-1.06 1.06l-4.5-4.5a.75.75 0 0 1 0-1.06l4.5-4.5a.75.75 0 0 1 1.06 1.06L4.56 7.25h8.69A.75.75 0 0 1 14 8Z" clipRule="evenodd" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="absolute -right-1 -top-1 size-4 rounded-full border border-base-content bg-base-200 p-0.5 text-base-content">
+                  <path fillRule="evenodd" d="M12.5 9.75A2.75 2.75 0 0 0 9.75 7H4.56l2.22 2.22a.75.75 0 1 1-1.06 1.06l-3.5-3.5a.75.75 0 0 1 0-1.06l3.5-3.5a.75.75 0 0 1 1.06 1.06L4.56 5.5h5.19a4.25 4.25 0 0 1 0 8.5h-1a.75.75 0 0 1 0-1.5h1a2.75 2.75 0 0 0 2.75-2.75Z" clipRule="evenodd" />
                 </svg>
+
 
               </div>
             </label>
