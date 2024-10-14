717c58c6a6902404b599ce7736326f65a940bfb6
unknown <felipescherer25@gmail.com>
2024-10-14 11:19:18 -0300

    :sparkles: feat: toolkit color border

--- src/app/[lang]/(LayoutWithHeaderFooter)/about/components/toolkit.component.tsx
+++ src/app/[lang]/(LayoutWithHeaderFooter)/about/components/toolkit.component.tsx
@@ -15,7 +15,7 @@ export function ToolkitComponent() {
             <label key={tool.name} className="swap swap-flip">
               <input type="checkbox" />
 
-              <div className="swap-off relative flex h-44 w-32 items-center justify-center rounded bg-neutral p-2 text-neutral-content">
+              <div className="swap-off relative flex h-44 w-32 items-center justify-center rounded border shadow-md ">
                 <div className="flex flex-col items-center gap-2 text-center">
 
                   <div className="rounded-md bg-white p-2">
@@ -23,13 +23,13 @@ export function ToolkitComponent() {
                   </div>
                   {tool.name}
 
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="absolute -right-1 -top-1 size-4 rounded-full  bg-base-content text-base-200">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="absolute -right-1 -top-1 size-4 rounded-full  bg-base-content text-base-100">
                     <path fillRule="evenodd" d="M15 8A7 7 0 1 1 1 8a7 7 0 0 1 14 0ZM9 5a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM6.75 8a.75.75 0 0 0 0 1.5h.75v1.75a.75.75 0 0 0 1.5 0v-2.5A.75.75 0 0 0 8.25 8h-1.5Z" clipRule="evenodd" />
                   </svg>
                 </div>
               </div>
 
-              <div className="swap-on relative flex h-44 w-32 flex-col items-center rounded bg-neutral p-2 text-neutral-content">
+              <div className="swap-on relative flex h-44 w-32 flex-col items-center rounded border p-2 shadow-md">
                 <span className="flex-1 text-xs">{tool.description}</span>
 
                 <Link href={tool.docLink} className="btn btn-circle btn-xs mt-4">
@@ -38,7 +38,7 @@ export function ToolkitComponent() {
                   </svg>
                 </Link>
 
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="absolute -right-1 -top-1 size-4 rounded-full border border-base-content bg-base-200 p-0.5 text-base-content">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="absolute -right-1 -top-1 size-4 rounded-full border border-base-content bg-base-100 p-0.5 text-base-content">
                   <path fillRule="evenodd" d="M12.5 9.75A2.75 2.75 0 0 0 9.75 7H4.56l2.22 2.22a.75.75 0 1 1-1.06 1.06l-3.5-3.5a.75.75 0 0 1 0-1.06l3.5-3.5a.75.75 0 0 1 1.06 1.06L4.56 5.5h5.19a4.25 4.25 0 0 1 0 8.5h-1a.75.75 0 0 1 0-1.5h1a2.75 2.75 0 0 0 2.75-2.75Z" clipRule="evenodd" />
                 </svg>
               </div>
